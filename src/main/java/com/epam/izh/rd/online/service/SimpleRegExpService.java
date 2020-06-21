0f01f1958fd67640760b0de5f572c06e88b919c9
Pavel Antropov <p.m.antropov@gmail.com>
2020-06-21 23:19:49 +0400

    Wrote methods for SimpleRegExpService

--- src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
@@ -1,5 +1,10 @@
 package com.epam.izh.rd.online.service;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class SimpleRegExpService implements RegExpService {
 
     /**
@@ -11,17 +16,55 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String maskSensitiveData() {
-        return null;
+
+        String textFromFile = "";
+
+        try (FileReader fileReader = new FileReader("./src/main/resources/sensitive_data.txt")) {
+
+            Scanner scanner = new Scanner(fileReader);
+
+            while (scanner.hasNextLine()) {
+                textFromFile += scanner.nextLine();
+            }
+
+            textFromFile = textFromFile.replaceAll("(?<=\\w\\s)(\\d{4}(?!\\s\\W))", "****");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return textFromFile;
     }
 
     /**
-     * Метод должен считыввать файл sensitive_data.txt (из директории resources) и заменять плейсхолдер ${payment_amount} и ${balance} на заданные числа. Метод должен
+     * Метод должен считывать файл sensitive_data.txt (из директории resources) и заменять плейсхолдер ${payment_amount} и ${balance} на заданные числа. Метод должен
      * содержать регулярное выражение для поиска плейсхолдеров
      *
      * @return обработанный текст
      */
     @Override
     public String replacePlaceholders(double paymentAmount, double balance) {
-        return null;
+
+        String textFromFile = "";
+
+        String paymentAmountFormatted = BigDecimal.valueOf(paymentAmount).stripTrailingZeros().toString();
+        String balanceFormatted = BigDecimal.valueOf(balance).stripTrailingZeros().toString();
+
+        try (FileReader fileReader = new FileReader("./src/main/resources/sensitive_data.txt")) {
+
+            Scanner scanner = new Scanner(fileReader);
+
+            while (scanner.hasNextLine()) {
+                textFromFile += scanner.nextLine();
+            }
+
+            textFromFile = textFromFile.replaceAll("(\\$\\{payment_amount})", paymentAmountFormatted);
+            textFromFile = textFromFile.replaceAll("(\\$\\{balance})", balanceFormatted);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return textFromFile;
     }
 }

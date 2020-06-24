9b308d769d171893fe7adefd15572b8aea7c2f4f
Pavel Antropov <p.m.antropov@gmail.com>
2020-06-24 11:07:10 +0400

    Added method to get files in directory in resources folder to my FileService

--- src/main/java/com/epam/izh/rd/online/service/FileServiceImpl.java
+++ src/main/java/com/epam/izh/rd/online/service/FileServiceImpl.java
@@ -1,10 +1,11 @@
 package com.epam.izh.rd.online.service;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class FileServiceImpl {
+public class FileServiceImpl implements FileService {
 
     public String readTextFromFile(String filePath) {
         String textFromFile = "";
@@ -22,5 +23,21 @@ public class FileServiceImpl {
         }
 
         return textFromFile;
+    }
+
+    @Override
+    public File[] getFilesInDirectoryInsideResourcesFolder(String directory) {
+
+        File givenDirectory = new File("./src/main/resources/" + directory);
+        if (!givenDirectory.exists()) {
+            givenDirectory = new File(directory);
+        }
+
+        File[] filesInDirectory = new File[0];
+        if (givenDirectory.isDirectory()) {
+            filesInDirectory = givenDirectory.listFiles();
+        }
+
+        return filesInDirectory;
     }
 }
